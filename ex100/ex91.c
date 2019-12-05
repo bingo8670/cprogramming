d9e53b49df8a177899bc029f437cd438f7b22d7c
bingo8670 <307286130@qq.com>
2019-12-05 13:42:50 +0800

    add ex--101

---
+++ ex100/ex91.c
@@ -0,0 +1,16 @@
+/*题目：时间函数举例1*/
+
+#include <stdio.h>
+#include <time.h>
+
+int main ()
+{
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+    printf ( "当前本地时间为: %s", asctime (timeinfo) );
+
+    return 0;
+}
